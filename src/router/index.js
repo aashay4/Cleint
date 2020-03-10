8c8f96a56af31418357e93ed7abc18ebeee9a20e
Aashay <aashay.trivedi1@gmail.com>
2020-03-10 18:19:24 +0530

    second

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import store from '../store'
-import About from '../views/about.vue'
 
 Vue.use(VueRouter)
 
@@ -15,7 +14,6 @@ const routes = [
   {
     path: '/about/',
     name: 'About',
-    component: About,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
